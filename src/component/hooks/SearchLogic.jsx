e3c5f5583b4d33fb04747c604044e9a83edd7fb1
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-03 16:10:00 +0100

    added dotenv

---
+++ src/component/hooks/SearchLogic.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import axios from 'axios'
+
+const SearchLogic = () => {
+  const clientId = process.env.NEXT_PUBLIC_UNSPLASH_CLIENT_ID
+
+  const UNSPLASH_ROOT = 'https://api.unsplash.com'
+
+  export const getPhotosByQuery = async ({ query }: { query: string }) => {
+    const { data } = await axios.get(
+      `${UNSPLASH_ROOT}/search/photos?query=${query}&client_id=${clientId}&per_page=20`
+    )
+    return data
+  }
+
+  return <div>SearchLogic</div>
+}
+
+export default SearchLogic
