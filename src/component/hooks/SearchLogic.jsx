0a1b0d25245fe503339196dd0df77c0f96b054ff
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-03 16:20:05 +0100

    added and wrapped the project with query  and more

--- src/component/hooks/SearchLogic.jsx
+++ src/component/hooks/SearchLogic.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const SearchLogic = () => {
-  const clientId = process.env.NEXT_PUBLIC_UNSPLASH_CLIENT_ID
-
-  const UNSPLASH_ROOT = 'https://api.unsplash.com'
-
-  export const getPhotosByQuery = async ({ query }: { query: string }) => {
-    const { data } = await axios.get(
-      `${UNSPLASH_ROOT}/search/photos?query=${query}&client_id=${clientId}&per_page=20`
-    )
-    return data
-  }
-
-  return <div>SearchLogic</div>
+  // const clientId = process.env.NEXT_PUBLIC_UNSPLASH_CLIENT_ID
+  // const UNSPLASH_ROOT = 'https://api.unsplash.com'
+  // export const getPhotosByQuery = async ({ query }: { query: string }) => {
+  //   const { data } = await axios.get(
+  //     `${UNSPLASH_ROOT}/search/photos?query=${query}&client_id=${clientId}&per_page=20`
+  //   )
+  //   return data
+  // }
+  // return <div>SearchLogic</div>
 }
 
 export default SearchLogic
